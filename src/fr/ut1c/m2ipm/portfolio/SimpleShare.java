71de71fb9d7005c1f2469d8c229639da4120edf5
DavidNavarreICS <navarredavid31@gmail.com>
2019-02-27 14:45:26 +0100

    Towards SimpleShare with zero default and hundred purcent coverage

--- src/fr/ut1c/m2ipm/portfolio/SimpleShare.java
+++ src/fr/ut1c/m2ipm/portfolio/SimpleShare.java
@@ -3,25 +3,52 @@ package fr.ut1c.m2ipm.portfolio;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A simple share is a share that holds a list of prices defined amongst a set
+ * of days.
+ *
+ * @see Share
+ * @see Day
+ * @see Price
+ * @author David Navarre
+ */
 public class SimpleShare extends Share {
 
+    /**
+     * A collection that maps a day into a price.
+     */
     private Map<Day, Price> mapPrices;
 
-    public SimpleShare(String name) {
+    /**
+     * Build a simple share.
+     *
+     * @param name a non null string
+     */
+    public SimpleShare(final String name) {
         super(name);
         this.mapPrices = new HashMap();
     }
 
-    public void setPrice(Day day, float value) {
-        if (this.mapPrices.containsKey(day) == false) {
-            this.mapPrices.put(day, new Price(day, value));
+    /**
+     * Defines the price for a given day if the price is not already defined.
+     * This method is not designed to be overriden.
+     *
+     * @param aDay a non null day
+     * @param aValue a strictly positive value
+     */
+    public final void setPrice(final Day aDay, final float aValue) {
+        if (!this.mapPrices.containsKey(aDay)) {
+            this.mapPrices.put(aDay, new Price(aDay, aValue));
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public float getPrice(Day day) {
-        if (this.mapPrices.containsKey(day) == true) {
-            return this.mapPrices.get(day).getValue();
+    public final float getPrice(final Day aDay) {
+        if (this.mapPrices.containsKey(aDay)) {
+            return this.mapPrices.get(aDay).getValue();
         } else {
             return 0.0F;
         }
--- test/fr/ut1c/m2ipm/portfolio/SimpleShareTest.java
+++ test/fr/ut1c/m2ipm/portfolio/SimpleShareTest.java
@@ -6,6 +6,7 @@
 package fr.ut1c.m2ipm.portfolio;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,6 +15,13 @@ import org.junit.Test;
  * @author David
  */
 public class SimpleShareTest {
+
+    private static final String WELL_FORMED_NAME = "name1";
+    private static final String MALFORMED_NAME = "";
+    private static final String NULL_NAME = null;
+    private static final Day DEFAULT_DAY = new Day(1, 1);
+    private static final float DEFAULT_VALUE1 = 1.0F;
+    private static final float DEFAULT_VALUE2 = 2.0F;
 
     public SimpleShareTest() {
     }
@@ -27,7 +35,41 @@ public class SimpleShareTest {
     }
 
     @Test
-    public void testConstructor() {
-        SimpleShare simpleShare = new SimpleShare("test");
+    public void testConstructorWithCorrectParameter() {
+        new SimpleShare(WELL_FORMED_NAME);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorWithInorrectParameter() {
+        new SimpleShare(MALFORMED_NAME);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorWithNullParameter() {
+        new SimpleShare(NULL_NAME);
+    }
+
+    @Test
+    public void testSetGetPriceWithCorrectValues() {
+        SimpleShare share = new SimpleShare(WELL_FORMED_NAME);
+        share.setPrice(DEFAULT_DAY, DEFAULT_VALUE1);
+        float value = share.getPrice(DEFAULT_DAY);
+        Assert.assertEquals(DEFAULT_VALUE1, value, 0.0F);
+    }
+
+    @Test
+    public void testSetGetPriceTwiceWithCorrectValues() {
+        SimpleShare share = new SimpleShare(WELL_FORMED_NAME);
+        share.setPrice(DEFAULT_DAY, DEFAULT_VALUE1);
+        share.setPrice(DEFAULT_DAY, DEFAULT_VALUE2);
+        float value = share.getPrice(DEFAULT_DAY);
+        Assert.assertEquals(DEFAULT_VALUE1, value, 0.0F);
+    }
+
+    @Test
+    public void testGetPriceWhenNotDefined() {
+        SimpleShare share = new SimpleShare(WELL_FORMED_NAME);
+        float value = share.getPrice(DEFAULT_DAY);
+        Assert.assertEquals(0.0F, value, 0.0F);
     }
 }
