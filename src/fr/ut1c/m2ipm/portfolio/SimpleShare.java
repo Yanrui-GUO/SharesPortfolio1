1435f450d23e347d5975e1f013cad2384f69a50e
DavidNavarreICS <navarredavid31@gmail.com>
2019-02-28 08:54:18 +0100

    Update Headers

--- src/fr/ut1c/m2ipm/portfolio/SimpleShare.java
+++ src/fr/ut1c/m2ipm/portfolio/SimpleShare.java
@@ -1,9 +1,21 @@
 /*
-* SharesPortfolio project is dedicated to teaching multiple purposes.
- * Copyright (C) 2019 M2 MIAGE IPM
+ * Portfolio, open source software.
+ * Copyright (C) 2014-2019 SonarSource
+ * mailto:navarre@irit.fr
  *
- * SharesPortfolio is a free software that does not do usefull things.
- * Created on : 28-02-2019
+ * Portfolio is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Portfolio is distributed in the hope that it will be useless,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 package fr.ut1c.m2ipm.portfolio;
 
